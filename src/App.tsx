662a44fd23d89504fcfd477b7411f1f339a18390
dorothy <dorothy.x.ren@gmail.com>
2024-06-13 11:27:15 -0400

    created React TTT

--- src/App.tsx
+++ src/App.tsx
@@ -6,33 +6,84 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  //keep track of the state by adding another state to the Board
+  const [xisNext, setXIsNext] = useState(true);
   //this declares a state variable named squares that defaults to an array of 9 nulls corresponding to the 9 squares
   const [squares, setSquares] = useState(Array(9).fill(null))
 
+
+  //creating a status section to the Board component
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Move: " + (xisNext ? "X" : "O");
+  }
+
+
   //The handleClick function creates a copy of the squares array (nextSquares) with the JavaScript slice() Array method. 
   //Then, handleClick updates the nextSquares array to add X to the first ([0] index) square.
-  function handleClick() {
+  //state handling is now in the parent Board component, and the Board passes props to the child Square components so they 
+  //display correctly. When clicking on a Square, the child square component asks the parent Board component to update
+  //the state of the board. When the Board's state changes, both the Board component and every child Square re-renders automatically
+
+  function handleClick(i) {
+
+    // || is an Or
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[0] = "X";
+    if (xisNext) {
+      nextSquares[i] = "X";
+    }
+    else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xisNext)
+
+  }
+
+  function calculateWinner(squares) {
+    const winOptions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < winOptions.length; i++) {
+      const [a, b, c] = winOptions[i]
+      if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
-        < Square value={squares[0]} onSquareClick={handleClick} />
-        < Square value={squares[1]} />
-        < Square value={squares[2]} />
+        < Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        < Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        < Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div>
-        < Square value={squares[3]} />
-        < Square value={squares[4]} />
-        < Square value={squares[5]} />
+        < Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        < Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        < Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div>
-        < Square value={squares[6]} />
-        < Square value={squares[7]} />
-        < Square value={squares[8]} />
+        < Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        < Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        < Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   )
